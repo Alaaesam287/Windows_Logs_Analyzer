4a4a4b40336c53dc0efcf8709ab609f5a593f519
Alaaesam287 <127038495+Alaaesam287@users.noreply.github.com>
2024-12-22 14:41:26 +0200

    Window1.xaml.cs

---
+++ Window1.xaml.cs
@@ -0,0 +1,74 @@
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Axes;
+using System.Windows;
+
+
+namespace Log_Analyzer
+{
+    public partial class Window1 : Window
+    {
+        public Window1(List<(DateTime timestamp, double cpuUsage, double memoryUsage)> cpuMemoryData)
+        {
+            InitializeComponent();
+
+            // Create the plot model
+            var plotModel = new PlotModel { Title = "CPU and Memory Usage Over Time" };
+
+            // Configure the time axis (X-axis)
+            plotModel.Axes.Add(new DateTimeAxis
+            {
+                Position = AxisPosition.Bottom,
+                Title = "Time",
+                StringFormat = "HH:mm",
+                Minimum = DateTimeAxis.ToDouble(cpuMemoryData[0].timestamp),
+                Maximum = DateTimeAxis.ToDouble(cpuMemoryData[cpuMemoryData.Count - 1].timestamp),
+                MajorGridlineStyle = LineStyle.Solid,
+                TextColor = OxyColors.White,
+                TitleColor = OxyColors.White
+            });
+
+            // Configure the primary Y-axis (CPU Usage)
+            plotModel.Axes.Add(new LinearAxis
+            {
+                Position = AxisPosition.Left,
+                Title = "Usage (%)",
+                Minimum = 0,
+                Maximum = 100,
+                TextColor = OxyColors.White,
+                TitleColor = OxyColors.White
+            });
+
+            // Create the CPU usage series
+            var cpuSeries = new LineSeries
+            {
+                Title = "CPU Usage (%)", // Legend entry
+                MarkerType = MarkerType.Circle,
+                Color = OxyColors.Red
+            };
+
+            // Create the Memory usage series
+            var memorySeries = new LineSeries
+            {
+                Title = "Memory Usage (%)", // Legend entry
+                MarkerType = MarkerType.Circle,
+                Color = OxyColors.Green
+            };
+
+            // Populate the series with data
+            foreach (var data in cpuMemoryData)
+            {
+                cpuSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(data.timestamp), data.cpuUsage));
+                memorySeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(data.timestamp), data.memoryUsage));
+            }
+
+            // Add the series to the plot model
+            plotModel.Series.Add(cpuSeries);
+            plotModel.Series.Add(memorySeries);
+
+            // Assign the plot model to the PlotView
+            PlotView.Model = plotModel;
+        }
+
+    }
+}
